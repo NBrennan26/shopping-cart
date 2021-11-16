c94ef9d06617f6505b341c8e30f7a62484ddcc1c
NBrennan26 <NBrennan26@gmail.com>
2021-11-16 16:13:22 -0600

    Added classname to NotFound so it displays correctly below the header

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function NotFound() {
   return (
-    <div id="not-found">
+    <div className="store-component" id="not-found">
       <h2>404 Page Not Found</h2>
       <h3>Something went Wrong</h3>
     </div>
