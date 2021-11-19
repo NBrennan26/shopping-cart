ce19124646d16b51c3b47755c49645bdcc77a045
NBrennan26 <NBrennan26@gmail.com>
2021-11-19 12:30:55 -0600

    GitHub link now opens in new tab

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ function About() {
       <p>We're just a simple Mom and Pop Shop that sells letters.</p>
       <p>Check out more of our stores and projects here:</p>
       <span id="link-span">
-        <a href="https://github.com/NBrennan26">
+        <a href="https://github.com/NBrennan26" target="_blank" rel="noreferrer">
           <img id="github-img" src={github} alt="github logo" />
         </a>
       </span>
